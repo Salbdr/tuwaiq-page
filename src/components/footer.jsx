4a32cf7d07768f8ee6fa4fa67cd483815170149a
sult <168359467+Salbdr@users.noreply.github.com>
2024-06-17 04:17:22 +0300

    Footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,57 @@
 export default function Footer() {
   return (
     <>
-      <footer></footer>
+      <div>
+        <footer class="footer">
+          <div class="footer-div">
+            <img src="https://tuwaiq.edu.sa/img/logos/logo.svg" alt="" />
+            <div>
+              <ul class="uls">
+                <li>شارك كمدرب</li>
+                <li>حول الأكاديمية</li>
+              </ul>
+            </div>
+            <div>
+              <ul class="uls">
+                <li>الشروط والأحكام</li>
+                <li>سياسة الخصوصية</li>
+              </ul>
+            </div>
+            <div>
+              <ul class="img-ul uls">
+                <li>
+                  <img
+                    src="https://tuwaiq.edu.sa/icons/discord-icon.svg"
+                    alt=""
+                  />
+                  <img
+                    src="https://tuwaiq.edu.sa/icons/twitter-icon.svg"
+                    alt=""
+                  />
+                  <img
+                    src="https://tuwaiq.edu.sa/icons/youtube-icon.svg"
+                    alt=""
+                  />
+                  <img
+                    src="https://tuwaiq.edu.sa/icons/snapchat-icon.svg"
+                    alt=""
+                  />
+                  <img
+                    src="https://tuwaiq.edu.sa/icons/linkedin-icon.svg"
+                    alt=""
+                  />
+                </li>
+                <li>@TuwaiqAcademy</li>
+              </ul>
+            </div>
+
+            <img src="https://tuwaiq.edu.sa/img/logos/logos2.svg" alt="" />
+          </div>
+          <div class="mb">
+            <span>جميع الحقوق محفوظة لأكاديمية طويق 2024</span>
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
